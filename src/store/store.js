b12b67f88c4bf2d2f247cf76840f2e43ec632e4c
Rakesh799 <rakeshchoudhury074@gmail.com>
2024-12-31 14:29:24 +0530

    removed comments

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,9 @@
-// we can make store in root folder or in src folder as per our confort.
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 
 const store = configureStore({
     reducer: {
-        auth: authSlice
+        auth: authSlice,
     }
 })
 
