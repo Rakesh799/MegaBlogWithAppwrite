fc1eea47fe85ed9e2700481677a83b64a75eda55
Rakesh799 <rakeshchoudhury074@gmail.com>
2025-01-13 15:54:25 +0530

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='w-12 flex items-center '>
+    <div className='w-14 flex items-center '>
       <img src="logo.png" alt="LOGO" />
       <img src="two.png" alt="LOGO2" />
-      
     </div>
   )
 }
