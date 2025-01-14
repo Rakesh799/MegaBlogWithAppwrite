cb2fb934d782aed7418adabc54f286019f1e976e
Rakesh799 <rakeshchoudhury074@gmail.com>
2025-01-14 20:52:00 +0530

    css updates

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom'
 const PostCard = ({ $id, title, featuredImage }) => {
     return (
         <Link to={`/post/${$id}`}>
-            <div className="w-full rounded-xl p-4 bg-gray-200 shadow-lg shadow-purple-800">
-                <div className="w-full mb-4 h-36 overflow-hidden rounded-xl">
+            <div className="w-full h-60 rounded-xl p-4 bg-gray-200 shadow-lg shadow-purple-800">
+                <div className="w-full mb-4 h-30 overflow-hidden rounded-xl">
                     <img src={service.getFilePreview(featuredImage)} alt={title} className='rounded-xl object-cover' />
                 </div>
                 <h2 className='text-xl font-bold'>{title}</h2>
