2420c66ee2c4713a30622bc2e520b922875fdc7f
Rakesh799 <rakeshchoudhury074@gmail.com>
2025-01-15 10:47:15 +0530

    styling changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
                 <Container>
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                            <h1 className="text-2xl font-bold hover:text-purple-950">
                                 Login to read posts
                             </h1>
                         </div>
