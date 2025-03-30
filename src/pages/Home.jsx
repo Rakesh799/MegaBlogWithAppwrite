2a1897a2d9af629753385e3a48ace9cf411ee9b6
Rakesh799 <rakeshchoudhury074@gmail.com>
2025-03-31 00:05:40 +0530

    updated message for new users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import service from '../appwrite/config'
 import { Container, PostCard } from '../components/index'
+import { motion } from "framer-motion";
 
 
 
@@ -20,9 +21,18 @@ const Home = () => {
                 <Container>
                     <div className="flex flex-wrap">
                         <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-[#4E342E]">
-                                Login to read posts
+                            <h1 className="text-4xl mb-8 font-bold hover:text-[#4E342E]">
+                                Welcome to MegaBlog 2025
                             </h1>
+
+                            <motion.div
+                                initial={{ scale: 0, opacity: 0 }} // Start hidden & small
+                                animate={{ scale: 1, opacity: 1 }} // Pop in effect
+                                transition={{ type: "spring", stiffness: 120, damping: 10, repeat: 6, repeatType: "reverse" }} // Smooth spring effect
+                                className="w-1/2 mx-auto transform -translate-x-1/2 bg-[#8b322c] text-white py-1 rounded-lg shadow-lg text-center font-bold text-xl"
+                            >
+                                Login to read posts | Signup if you are a new user
+                            </motion.div>
                         </div>
                     </div>
                 </Container>
